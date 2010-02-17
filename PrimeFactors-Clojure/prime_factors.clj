8041843679fa6706823ffd949f9d7818580d4f84
sroccaserra <sroccaserra@yahoo.com>
2010-02-17 16:16:34 +0100

    Fixed indentation in prime factors func

--- PrimeFactors-Clojure/prime_factors.clj
+++ PrimeFactors-Clojure/prime_factors.clj
@@ -4,7 +4,8 @@
   (loop [n number
          factors []
          divisor 2]
-    (if (> divisor (Math/sqrt n)) (conj factors n)
+    (if (> divisor (Math/sqrt n))
+      (conj factors n)
       (if (not= 0 (mod n divisor))
         (recur n factors (inc divisor))
         (recur (/ n divisor) (conj factors divisor) divisor)))))
