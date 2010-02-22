19aba56b2d1e75cf278d895dcf70fbe61911c363
sroccaserra <sroccaserra@yahoo.com>
2010-02-22 14:20:14 +0100

    Added comments

--- PrimeFactors-Clojure/prime_factors.clj
+++ PrimeFactors-Clojure/prime_factors.clj
@@ -1,11 +1,9 @@
 (ns prime-factors)
 
-(defn factors-of [number]
-  (loop [n number
-         factors []
-         divisor 2]
-    (if (> divisor (Math/sqrt n))
-      (conj factors n)
-      (if (not= 0 (mod n divisor))
-        (recur n factors (inc divisor))
-        (recur (/ n divisor) (conj factors divisor) divisor)))))
+(defn factorization [n]
+  (loop [number n
+         divisor 2
+         factors []]
+     (cond (> divisor number) factors
+           (= 0 (mod number divisor)) (recur (/ number divisor) divisor (conj factors divisor))
+           true (recur number (inc divisor) factors))))
