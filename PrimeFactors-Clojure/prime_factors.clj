c5469496289e5427e4337d93d2a8a76bc748c241
sroccaserra <sroccaserra@yahoo.com>
2010-02-24 15:34:44 +0100

    Added README

--- PrimeFactors-Clojure/prime_factors.clj
+++ PrimeFactors-Clojure/prime_factors.clj
@@ -5,6 +5,10 @@
          divisor 2
          factors []]
     (cond (= 1 number)                   factors
-          (= 0 (mod number divisor))     (recur (/ number divisor) divisor (conj factors divisor))
-          (< divisor (Math/sqrt number)) (recur number (inc divisor) factors)
+          (= 0 (mod number divisor))     (recur (/ number divisor)
+                                                divisor
+                                                (conj factors divisor))
+          (< divisor (Math/sqrt number)) (recur number
+                                                (inc divisor)
+                                                factors)
           true                           (conj factors number))))
---
+++ README.md
@@ -0,0 +1,1 @@
+Some kata preparations for the [Paris coding dojo](http://xp-france.net/cgi-bin/wiki.pl?DojoDeveloppement).
