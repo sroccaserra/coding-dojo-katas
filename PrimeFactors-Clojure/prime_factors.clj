28347a19d469b534f800cbc2f17666e62de04545
sroccaserra <sroccaserra@yahoo.com>
2010-02-24 15:29:00 +0100

    small format

--- PrimeFactors-Clojure/prime_factors.clj
+++ PrimeFactors-Clojure/prime_factors.clj
@@ -4,7 +4,7 @@
   (loop [number n
          divisor 2
          factors []]
-    (cond (= 1 number) factors
-          (= 0 (mod number divisor)) (recur (/ number divisor) divisor (conj factors divisor))
+    (cond (= 1 number)                   factors
+          (= 0 (mod number divisor))     (recur (/ number divisor) divisor (conj factors divisor))
           (< divisor (Math/sqrt number)) (recur number (inc divisor) factors)
-          true                       (conj factors number))))
+          true                           (conj factors number))))
