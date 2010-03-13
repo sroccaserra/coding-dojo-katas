e712e172e67a1684c36db76ddc8063413f714207
sroccaserra <sroccaserra@yahoo.com>
2010-03-13 19:54:59 +0100

    Simpler Erlang prime factors

--- Prime_Factors-Erlang/src/prime_factors.erl
+++ Prime_Factors-Erlang/src/prime_factors.erl
@@ -7,8 +7,7 @@ factors(X) -> reverse(factors(X, 2, [])).
 
 factors(X, Divisor, Factors) when Divisor > X ->
     Factors;
+factors(X, Divisor, Factors) when X rem Divisor == 0 ->
+    factors(X div Divisor, Divisor, [Divisor|Factors]);
 factors(X, Divisor, Factors) ->
-    case X rem Divisor of
-        0 -> factors(X div Divisor, Divisor, [Divisor|Factors]);
-        _ -> factors(X, Divisor + 1, Factors)
-    end.
+    factors(X, Divisor + 1, Factors).
