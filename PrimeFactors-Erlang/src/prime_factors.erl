6522a43ecfce251cbd098a1a1ad09cadf39ec841
sroccaserra <sroccaserra@yahoo.com>
2010-03-15 22:35:43 +0100

    Small work on PrimeFactors-Erlang

--- PrimeFactors-Erlang/src/prime_factors.erl
+++ PrimeFactors-Erlang/src/prime_factors.erl
@@ -5,12 +5,12 @@
 
 factors(X) -> reverse(factors(X, 2, [])).
 
-factors(1, Divisor, Factors) -> Factors;
+factors(1, _, Factors) -> Factors;
 factors(X, Divisor, Factors) when X rem Divisor == 0 ->
     factors(X div Divisor, Divisor, [Divisor|Factors]);
 factors(X, Divisor, Factors) ->
-    B = Divisor > sqrt(X),
+    Sqrt = sqrt(X),
     if
-        B -> [X|Factors];
+        Divisor > Sqrt -> [X|Factors];
         true -> factors(X, Divisor + 1, Factors)
     end.
