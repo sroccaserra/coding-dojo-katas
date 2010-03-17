6a455fa73baf9767bbe02ca144546cfdcdb1dc26
sroccaserra <sroccaserra@yahoo.com>
2010-03-17 14:06:46 +0100

    used identical operator in erlang kata

--- PrimeFactors-Erlang/src/prime_factors.erl
+++ PrimeFactors-Erlang/src/prime_factors.erl
@@ -5,12 +5,13 @@
 
 factors(N) -> reverse(factors(N, 2, [])).
 
-factors(1, _, Factors) -> Factors;
-factors(N, Divisor, Factors) when N rem Divisor == 0 ->
+factors(1, _Divisor, Factors) -> Factors;
+
+factors(N, Divisor, Factors) when N rem Divisor =:= 0 ->
     factors(N div Divisor, Divisor, [Divisor|Factors]);
+
 factors(N, Divisor, Factors) ->
     case Divisor > sqrt(N) of
         true -> [N|Factors];
         false -> factors(N, Divisor + 1, Factors)
     end.
-
