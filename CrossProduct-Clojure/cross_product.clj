6ef572e9b793ba08aed6018015cd3fbd02cc0cb2
sroccaserra <sroccaserra@yahoo.com>
2010-03-23 17:26:25 +0100

    rename in zip function

--- CrossProduct-Clojure/cross_product.clj
+++ CrossProduct-Clojure/cross_product.clj
@@ -1,10 +1,10 @@
 (ns cross_product)
 
-(defn zip-in-plist [names values]
-  "Zips names and values in a plist, in the Emacs sense (see tests)."
-  (let [name-value-map (zipmap names
-                               values)]
-    (reduce concat (sort name-value-map))))
+(defn zip-in-plist [keys values]
+  "Zips keys and values in a plist, in the Emacs sense (see tests)."
+  (let [ziped-map (zipmap keys
+                          values)]
+    (reduce concat (sort ziped-map))))
 
 (defmacro cross-product [& arrays]
   "Just wraps a call to Clojure's for macro."
