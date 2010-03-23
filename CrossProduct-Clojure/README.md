2b31903e60d0cc8293171f9dde684ff09e64c2c7
sroccaserra <sroccaserra@yahoo.com>
2010-03-23 14:29:30 +0100

    a few test renames, and completed the readme a bit

--- CrossProduct-Clojure/README.md
+++ CrossProduct-Clojure/README.md
@@ -1,1 +1,3 @@
 The goal of this kata is to create a function (or macro) that accepts any number of vectors in imput, and returns the set of all combinations.
+
+This was inspired by [yesterday's Paris Coding Dojo session](http://xp-france.net/cgi-bin/wiki.pl?DojoDeveloppement/Lundi22Mars2010), where Brian Marick proposed and performed this kata. I wanted to try it by just wrapping a call to Clojure's list comprehension (or `for` macro).
--- CrossProduct-Clojure/test.clj
+++ CrossProduct-Clojure/test.clj
@@ -1,22 +1,30 @@
 (ns test
     (:use clojure.test cross_product))
 
-(deftest testing-plist
+(deftest plist-with-one-value
+  (is (= [:a 1]
+         (plist [:a] [1]))))
+
+(deftest plist-with-two-values
   (is (= [:a 1 :b 2]
          (plist [:a :b] [1 2]))))
 
-(deftest simple
-  (is (= [[1 2] [1 3]]
-         (cp [1] [2 3]))))
-
 (deftest testing-nil
   (is (= []
-         (cp [2 3] []))))
+         (cp [1 2] []))))
 
-(deftest testing-identity
+(deftest one-array
   (is (= [[1] [2] [3]]
          (cp [1 2 3]))))
 
-(deftest testing-three-arrays
+(deftest two-arrays
+  (is (= [[1 2] [1 3]]
+         (cp [1] [2 3]))))
+
+(deftest three-arrays
   (is (= [[1 2 3] [1 2 4]]
          (cp [1] [2] [3 4]))))
+
+(deftest more-values
+  (is (= [[1 4] [1 5] [1 6] [2 4] [2 5] [2 6] [3 4] [3 5] [3 6]]
+         (cp [1 2 3] [4 5 6]))))
