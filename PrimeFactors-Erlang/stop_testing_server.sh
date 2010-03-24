a46596bb5be4c17ec1146a4d4ee93477ba4d6828
sroccaserra <sroccaserra@yahoo.com>
2010-03-24 13:50:04 +0100

    comments in erlang start script

--- PrimeFactors-Erlang/stop_testing_server.sh
+++ PrimeFactors-Erlang/stop_testing_server.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# erl -sname tom -eval "rpc:call(forge@$HOST, testing_server, stop, [])." \
+#  -eval "rpc:call(forge@$HOST, init, stop, [])." \
+#  -s init stop
+
 if [ -f ERLANG_TESTING_SERVER ]; then
     PID=`cat ERLANG_TESTING_SERVER`
     OLD_SERVER_INFO=`ps -p $PID | grep /erl$`
@@ -10,4 +14,3 @@ if [ -f ERLANG_TESTING_SERVER ]; then
     rm ERLANG_TESTING_SERVER
     rm log.txt
 fi
-
