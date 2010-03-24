2b137a594f6346817e0d4c32ad59612f825a1401
sroccaserra <sroccaserra@yahoo.com>
2010-03-24 13:43:47 +0100

    Fixed a typo

--- CrossProduct-Clojure/cross_product.clj
+++ CrossProduct-Clojure/cross_product.clj
@@ -2,13 +2,13 @@
 
 (defn zip-in-plist [keys values]
   "Zips keys and values in a plist, in the Emacs sense (see tests)."
-  (let [ziped-map (zipmap keys
+  (let [zipped-map (zipmap keys
                           values)]
-    (apply concat (sort ziped-map))))
+    (apply concat (sort zipped-map))))
 
 (defmacro cross-product [& arrays]
   "Just wraps a call to Clojure's for macro."
   (let [symbols (for [_ arrays]
                   (gensym))]
-    `(for [~@(zip-in-plist symbols arrays)] [~@symbols])))
-
+    `(for [~@(zip-in-plist symbols arrays)]
+       [~@symbols])))
